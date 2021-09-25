b53ff1ef102d456344d38d88b8b325d4590d3c3a
Atharv108 <83223565+Atharv108@users.noreply.github.com>
2021-09-25 02:52:35 -0700

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,55 @@
+// Primitive datatypes-:
+// 1. String
+var name = ("Atharv");
+console.log( "My string is "+ name);
+console.log("Datatype is " + (typeof name));
+
+ //2. Numbers                                                    
+var marks = (100);
+console.log( "My number is "+ marks);
+console.log('Datatype is ' + (typeof marks));
+
+// 3. Boolean Operator
+
+let isRead = true;
+console.log("Datatype is " + (typeof isRead));
+
+// 4.Null
+
+let nullVar = null;
+console.log("Datatype is " + (typeof nullVar));
+
+// 5. Undefined
+
+let undef = undefined;
+console.log("Datatype is " + (typeof undef));
+
+// Referecne Datatypes-:
+
+// 1. Array 
+// Array is collection of elements in a particular set.
+ let myarr = [3,4,5,6,7,false,"string"];
+console.log("Datatype is " + (typeof myarr));
+
+// 2. Object literals
+
+let stMarks = {
+    Atharv: 100,
+    Dev: 20,
+};
+console.log(stMarks);
+console.log("Datatype is " + (typeof stMarks));
+
+// 3. Functions
+function findName() {};
+console.log("Datatype is " + (typeof findName));
+
+// 4. Date
+let date = new Date();
+console.log("Datatype is " + (typeof date));
+
+
+
+
+
+
